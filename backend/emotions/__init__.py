ef514bf0a68bad0b6c20bd2846cc5c4601bbc1a7
EvilAv <andrey.vookolov85@gmail.com>
2025-02-09 01:32:29 +0300

    feat: add feature toggle to emotion recognition

--- backend/emotions/__init__.py
+++ backend/emotions/__init__.py
@@ -1,6 +1,12 @@
 from flask import Blueprint
-import nltk
+import os
 
 emotions = Blueprint('emotions', __file__, url_prefix='/emotion')
 
-from . import type, views, sybil
+
+if os.environ.get('EMOTION_RECOGNITION') == 'True':
+    from . import type, views, sybil
+    print('true prediction')
+else:
+    from . import type, views
+    print('random prediction')
--- backend/emotions/views.py
+++ backend/emotions/views.py
@@ -3,31 +3,39 @@ from flask import request
 from post.models import Post
 from emotions import emotions
 from emotions.type import Emotion
-from emotions.sybil import prepocess, word_to_vector
 from core import db
-import keras
 import numpy as np
+import random
 
-# for some reasons file can't be called types.py ¯\_(ツ)_/¯
-basedir = os.path.abspath(os.path.dirname(__file__))
-net = keras.models.load_model(os.path.join(basedir, 'bb.keras'))
+if os.environ.get('EMOTION_RECOGNITION') == 'True':
+    from emotions.sybil import prepocess, word_to_vector
+    import keras
+
+    # for some reasons file can't be called types.py ¯\_(ツ)_/¯
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    net = keras.models.load_model(os.path.join(basedir, 'bb.keras'))
 
 @emotions.route('/one')
 def get_one_emotion():
     post_id = request.args.get('post')
     if post_id:
         post = db.session.execute(db.select(Post).filter_by(id=int(post_id))).scalar_one_or_none()
-        if post and net:
-            # by the way there is lib for js for same thing
-            tokens = prepocess(post.text)
-            vec = [word_to_vector(t) for t in tokens]
-            # to fix shape
-            vec = np.array(vec, ndmin=3)
-            # print(vec.shape)
-            result = net.predict(vec)
-            print(result[0])
-            # cause result has 2 dimensions for some reason
+        if os.environ.get('EMOTION_RECOGNITION') == 'True':
+            if post and net:
+                # by the way there is lib for js for same thing
+                tokens = prepocess(post.text)
+                vec = [word_to_vector(t) for t in tokens]
+                # to fix shape
+                vec = np.array(vec, ndmin=3)
+                # print(vec.shape)
+                result = net.predict(vec)
+                print(result[0])
+                # cause result has 2 dimensions for some reason
+                return {
+                    'emotion': Emotion(np.argmax(result[0])).name
+                }
+        else:
             return {
-                'emotion': Emotion(np.argmax(result[0])).name
+                'emotion': Emotion(random.choice(range(5))).name
             }
     return {}, 404
